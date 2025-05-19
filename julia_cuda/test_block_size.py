1ac74594b839e8b10401d64ca4d92d23fbd42d3f
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-05-19 15:40:24 +0200

    Update test_block_size.py

--- julia_cuda/test_block_size.py
+++ julia_cuda/test_block_size.py
@@ -10,7 +10,8 @@ import random
 # Parameters for the scalability analysis
 problem_sizes = [1500]
 block_sizes = [(i, j) for i in range(1, 1025, 1) for j in range(16, 1025, 1) if i * j % 32 == 0]
-block_sizes = random.sample(block_sizes, 200, replace=False)  
+#sample from block_sizes into block_sizes 200 samples without replacement
+block_sizes = random.sample(block_sizes, 200)
 results = []
 raw_results = []
 
