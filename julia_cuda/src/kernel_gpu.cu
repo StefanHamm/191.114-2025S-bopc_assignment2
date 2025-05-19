d74da2732df010fd45b570c7bb92342fafed55b0
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-05-19 16:16:05 +0200

    Update kernel_gpu.cu

--- julia_cuda/src/kernel_gpu.cu
+++ julia_cuda/src/kernel_gpu.cu
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include "kernel.hpp"
+#include <cuda_runtime.h>
 
 extern int global_block_x, global_block_y;
 
@@ -32,6 +33,14 @@ __global__ void julia_kernel_worker(float *julia_set, Complex c, float scale, in
 void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y, int max_iter, float max_mag, float x_scale, float y_scale) {
 
     // compute a good default block size
+    int device;
+    cudaGetDevice(&device)
+
+    cudaDeviceProp prop;   
+    cudaGetDeviceProperties( &prop, device);
+
+    printf("Using device %d: %s\n", device, prop.name);
+    printf("Max threads per block: %d\n", prop.maxThreadsPerBlock);
 
     dim3 blockShape = dim3(global_block_x, global_block_y);
     dim3 gridShape = dim3( (res_x+blockShape.x-1)/blockShape.x,
