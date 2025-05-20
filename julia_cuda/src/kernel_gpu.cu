149d21c265e226fb1f3d17e80be3d4b5ddbdb48f
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-05-20 14:02:11 +0200

    Update kernel_gpu.cu

--- julia_cuda/src/kernel_gpu.cu
+++ julia_cuda/src/kernel_gpu.cu
@@ -43,6 +43,8 @@ void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y
     //max_block_size = prop.maxThreadsPerBlock;
 
 
+    cudaFree(0);
+
     dim3 blockShape;
     if (global_block_x == -1 && global_block_y == -1) {
         //blockShape = dim3(32, max_block_size/32);
